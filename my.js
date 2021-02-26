ef5ec14d837231ff58351bd22dec91f0b2a1acee
samod singh <samod0098@gmail.com>
2021-02-26 22:10:24 +0530

    veribele and data type

---
+++ my.js
@@ -0,0 +1,52 @@
+document.write("my test project <br>"); 
+document.write("my test project <br>"); 
+
+document.write("<i> my test project </i><br>"); // singal line coments
+/*document.write("dubel line comments test");
+document.write("dubel line comments test");*/  
+var x="Hii how are you <br>"; 
+
+ 
+ var first_name ="my village <br>";
+ var firstName ="pipra kala <br>";
+ var firstName12 ="palamu <br>";
+
+
+document.write(x);
+
+document.write(first_name);
+document.write(firstName);
+document.write(typeof x);
+document.write("<br>")
+
+var z=1000;
+document.write(typeof z);
+document.write(z);
+document.write("<br>")
+
+
+
+var a =["html", "css" ,"js"];
+document.write(typeof a);
+
+document.write("<br>")
+document.write(a);
+document.write("<br>")
+
+//assignment operators
+var s =10;
+var k=5;
+s *= k ;
+document.write(s)
+
+document.write("<br>")
+
+console.log(x)
+
+
+
+
+
+
+
+
